86fe78d97f7a58c432ff32edbcb6e33893e815e3
e <hhhhh_7417@qq.com>
2020-06-14 11:31:35 +0800

    芸众后台修改退款申请

--- resources/views/refund/index.blade.php
+++ resources/views/refund/index.blade.php
@@ -77,7 +77,7 @@
                 </div>
             </div>
         @endif
-        @if ($order['has_one_refund_apply']['is_refunding'])
+        @if ($order['has_one_refund_apply']['is_refunding'] || $order['has_one_refund_apply']['status']==8)
         <div class="form-group">
             <label class="col-xs-12 col-sm-3 col-md-2 control-label"> </label>
             <div class="col-sm-9 col-xs-12">
