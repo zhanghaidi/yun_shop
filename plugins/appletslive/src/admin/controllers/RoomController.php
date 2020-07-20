c873187cf53f6e0b5f2e8c2f375f988a5e5e7cea
李果果 <641785852@qq.com>
2020-07-20 11:41:13 +0800

    小程序直播管理

---
+++ plugins/appletslive/src/admin/controllers/RoomController.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: weifeng
+ * Date: 2019-12-18
+ * Time: 16:00
+ *
+ *    .--,       .--,
+ *   ( (  \.---./  ) )
+ *    '.__/o   o\__.'
+ *       {=  ^  =}
+ *        >  -  <
+ *       /       \
+ *      //       \\
+ *     //|   .   |\\
+ *     "'\       /'"_.-~^`'-.
+ *        \  _  /--'         `
+ *      ___)( )(___
+ *     (((__) (__)))     梦之所想,心之所向.
+ */
+
+namespace Yunshop\Appletslive\admin\controllers;
+
+use Illuminate\Support\Facades\DB;
+use app\common\components\BaseController;
+use app\common\facades\Setting;
+use Yunshop\Appletslive\common\services\BaseService;
+
+class RoomController extends BaseController
+{
+    // 房间列表
+    public function index()
+    {
+        $room_list = Cache::get('live_room_info');
+        if (empty($room_list)) {
+            $result = (new BaseService())->getRooms($this->getToken());
+
+            foreach ($result['room_info'] as &$room) {
+                $room['start_time'] = date('Y-m-d H:i:s', $room['start_time']);
+                $room['end_time'] = date('Y-m-d H:i:s', $room['end_time']);
+                if (strexists($room['cover_img'], 'http')) {
+                    $room['cover_img'] = str_replace('http', 'https', $room['cover_img']);
+                }
+                if (strexists($room['share_img'], 'http')) {
+                    $room['share_img'] = str_replace('http', 'https', $room['share_img']);
+                }
+                if (!empty($room['goods'])) {
+                    foreach ($room['goods'] as &$goods) {
+                        if (strexists($goods['cover_img'], 'http')) {
+                            $goods['cover_img'] = str_replace('http', 'https', $goods['cover_img']);
+                        }
+                    }
+                }
+            }
+
+            Cache::put('live_room_info', $result['room_info'], 3);
+            $room_list = Cache::get('live_room_info');
+        }
+
+        return view('Yunshop\Appletslive::admin.room_index', [
+            'room_list' => $room_list,
+        ])->render();
+    }
+
+    // 房间设置
+    public function set()
+    {
+        $form_data = request()->form_data;
+        if ($form_data) {
+            return $this->successJson('保存成功', $form_data);
+        }
+
+        $room_id = request()->get('room_id', 0);
+        $room_info = DB::table('users')->where('id', $room_id)->first();
+
+        return view('Yunshop\Appletslive::admin.room_set', [
+            'room_info' => $room_info,
+        ])->render();
+    }
+
+    // 回看列表
+    public function replaylist()
+    {
+        $room_id = request()->rid;
+        $result = (new BaseService())->getReplays($this->getToken(), $room_id);
+
+        return view('Yunshop\Appletslive::admin.replay_list', [
+            'list' => $result,
+        ])->render();
+    }
+}
---
+++ plugins/appletslive/views/admin/room_index.blade.php
@@ -0,0 +1,75 @@
+@extends('layouts.base')
+@section('title', trans('房间管理'))
+@section('content')
+
+    <div class="right-titpos">
+        <ul class="add-snav">
+            <li class="active"><a href="#">房间管理</a></li>
+        </ul>
+    </div>
+
+    <div class="panel panel-info">
+        <ul class="add-shopnav">
+            <li class="active">
+                <a href="">直播</a>
+            </li>
+            <li class="">
+                <a href="">录播</a>
+            </li>
+        </ul>
+    </div>
+
+    <div class='panel panel-default'>
+        <div class='panel-body'>
+
+            <table class="table table-hover" style="overflow:visible;">
+                <thead>
+                <tr>
+                    <th style='width:8%;'>ID</th>
+                    <th style='width:12%;'>封面</th>
+                    <th style='width:20%;'>标题</th>
+                    <th style='width:10%;'>开始时间</th>
+                    <th style='width:10%;'>结束时间</th>
+                    <th style='width:10%;'>报名人数</th>
+                    <th style='width:10%;'>直播状态</th>
+                    <th style='width:20%;'>操作</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($room_list as $row)
+                    <tr>
+                        <td>{{$row['roomid']}}</td>
+                        <td>
+                            <img src="{{$row['cover_img']}}" style="width: 30px; height: 30px;border:1px solid #ccc;padding:1px;">
+                        </td>
+                        <td>{{$row['name']}}</td>
+                        <td>{{date('Y-m-d H:i:s',$row['start_time'])}}</td>
+                        <td>{{date('Y-m-d H:i:s',$row['end_time'])}}</td>
+                        <td>99999999</td>
+                        <td>$row['live_status']</td>
+                        <td style="overflow:visible;">
+                            <a class='btn btn-default'
+                               href="{{yzWebUrl('plugin.appletslive.admin.controllers.room.set', ['roomid' => $row['roomid']])}}"
+                               title='房间设置'><i class='fa fa-edit'></i>房间设置
+                            </a>
+                            <a class='btn btn-default'
+                               href="{{yzWebUrl('plugin.appletslive.admin.controllers.room.replaylist', ['roomid' => $row['roomid']])}}"
+                               title='回看列表'><i class='fa fa-list'></i>回看列表
+                            </a>
+                        </td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <div style="width:100%;height:150px;"></div>
+    <script type="text/javascript">
+        $(function() {
+            $('#search').click(function () {
+                $('#form1').attr('action', '{!! yzWebUrl('plugin.commission.admin.agent.index') !!}');
+                $('#form1').submit();
+            });
+        });
+    </script>
+@endsection
