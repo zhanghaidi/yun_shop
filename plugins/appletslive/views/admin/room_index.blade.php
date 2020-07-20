f19716d3921fe14ba95e24f9a735d39800a9dac3
李果果 <641785852@qq.com>
2020-07-20 12:16:42 +0800

    直播房间列表

--- plugins/appletslive/views/admin/room_index.blade.php
+++ plugins/appletslive/views/admin/room_index.blade.php
@@ -28,11 +28,11 @@
                     <th style='width:8%;'>ID</th>
                     <th style='width:12%;'>封面</th>
                     <th style='width:20%;'>标题</th>
-                    <th style='width:10%;'>开始时间</th>
-                    <th style='width:10%;'>结束时间</th>
-                    <th style='width:10%;'>报名人数</th>
-                    <th style='width:10%;'>直播状态</th>
-                    <th style='width:20%;'>操作</th>
+                    <th style='width:15%;'>开始时间</th>
+                    <th style='width:15%;'>结束时间</th>
+                    <th style='width:8%;'>报名人数</th>
+                    <th style='width:7%;'>直播状态</th>
+                    <th style='width:15%;'>操作</th>
                 </tr>
                 </thead>
                 <tbody>
@@ -43,10 +43,10 @@
                             <img src="{{$row['cover_img']}}" style="width: 30px; height: 30px;border:1px solid #ccc;padding:1px;">
                         </td>
                         <td>{{$row['name']}}</td>
-                        <td>{{date('Y-m-d H:i:s',$row['start_time'])}}</td>
-                        <td>{{date('Y-m-d H:i:s',$row['end_time'])}}</td>
+                        <td>{{$row['start_time']}}</td>
+                        <td>{{$row['end_time']}}</td>
                         <td>99999999</td>
-                        <td>$row['live_status']</td>
+                        <td>{{$row['live_status']}}</td>
                         <td style="overflow:visible;">
                             <a class='btn btn-default'
                                href="{{yzWebUrl('plugin.appletslive.admin.controllers.room.set', ['roomid' => $row['roomid']])}}"
