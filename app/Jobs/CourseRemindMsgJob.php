f671592d87563bbac54aca461faf90aaf23c77ba
李果果 <641785852@qq.com>
2020-08-01 17:38:11 +0800

    课程提醒队列测试

--- app/Jobs/CourseRemindMsgJob.php
+++ app/Jobs/CourseRemindMsgJob.php
@@ -34,7 +34,7 @@ class CourseRemindMsgJob implements ShouldQueue
             'openid' => $openid,
             'miniApp' => ['miniprogram' => [
                 'appid' => 'wxcaa8acf49f845662',
-                'pagepath' => 'pages/CouRse/index?room_id=' . $room['id'],
+                'pagepath' => 'pages/template/rumours/index?room_id=' . $room['id'],
             ]],
         ];
     }
