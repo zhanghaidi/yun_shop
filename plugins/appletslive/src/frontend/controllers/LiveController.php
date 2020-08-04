5574e4cd76dbda23ecede25b12b964aa00b23ca2
李果果 <641785852@qq.com>
2020-08-04 16:48:20 +0800

    调试发送小程序模板消息

--- plugins/appletslive/src/frontend/controllers/LiveController.php
+++ plugins/appletslive/src/frontend/controllers/LiveController.php
@@ -133,61 +133,27 @@ class LiveController extends BaseController
     {
         $start_time = implode('.', array_reverse(explode(' ', substr(microtime(), 2))));
 
-        $account = AccountWechats::getAccountByUniacid(39);
-        $options = [
-            'app_id' => $account['key'],
-            'secret' => $account['secret'],
-        ];
-        // $app = new Application($options);
-        // $app = $app->notice;
-        $template_id = 'c-tYzcbVnoqT33trwq6ckW_lquLDPmqySXvntFJEMhE';
-        $notice_data = [
-            'first' => ['value' => '尊敬的用户,您订阅的课程有新视频要发布啦~', 'color' => '#173177'],
-                'keyword1' => ['value' => '【和大师一起学艾灸】', 'color' => '#173177'],
-                'keyword2' => ['value' => '长期有效', 'color' => '#173177'],
-                'keyword3' => ['value' => '更新中', 'color' => '#173177'],
-                'remark' => [
-                    'value' => '最新视频【每次艾灸几个穴位合适】将于2020-08-05 12:00震撼发布!',
-                    'color' => '#173177',
-                ],
-        ];
-        $openid = 'owVKQwWK2G_K6P22he4Fb2nLI6HI';
-        // $send = $app
-        //     ->uses($template_id)
-        //     ->andData($notice_data)
-        //     ->andReceiver($openid)
-        //     ->andUrl('')
-        //     ->send([]);
-        // $result['wechat'] = [
-        //     'easywechat_app' => $app,
-        //     'send' => $send,
-        // ];
-
-        $job = new SendTemplateMsgJob('wechat', $options, $template_id, $notice_data, $openid, '', '');
-        $dispatch = dispatch($job);
-        $result['wechat'] = ['job' => $job, 'dispatch' => $dispatch];
-
-        // $service = new SmallProgramNotice();
+        $service = new SmallProgramNotice();
         // $template_id = 'ABepy-L03XH_iU0tPd03VUV9KQ_Vjii5mClL7Qp8_jc';
-        $template_id = 'UKXQY-ReJezg0EHKvmp3yUQg-t644GNOaEIlV-Pqy84';
         // $notice_data = [
         //     'thing1' => ['value' => '课程更新', 'color' => '#173177'],
         //     'thing2' => ['value' => '【和大师一起学艾灸】', 'color' => '#173177'],
         //     'name3' => ['value' => '艾居益灸师', 'color' => '#173177'],
         //     'thing4' => ['value' => '最新视频【每次艾灸几个穴位合适】将在' . date('Y-m-d H:i', strtotime('+15 minutes')) . '震撼发布!', 'color' => '#173177'],
         // ];
+        $template_id = 'UKXQY-ReJezg0EHKvmp3yUQg-t644GNOaEIlV-Pqy84';
         $notice_data = [
             'thing1' => ['value' => '课程更新', 'color' => '#173177'],
             'thing2' => ['value' => '【和大师一起学艾灸】', 'color' => '#173177'],
             'time3' => ['value' => date('Y-m-d H:i', strtotime('+15 minutes')), 'color' => '#173177'],
         ];
         $openid = 'oP9ym5Bxp6D_sERpj340uIxuaUIo';
-        // $page = 'pages/template/rumours/index?room_id=5';
-        // $send = $service->sendSubscribeMessage($template_id, $notice_data, $openid, $page);
-        // $result['wxapp'] = [
-        //     'service' => $service,
-        //     'send' => $send,
-        // ];
+        $page = 'pages/template/rumours/index?room_id=5';
+        $send = $service->sendSubscribeMessage($template_id, $notice_data, $openid, $page);
+        $result['wxapp'] = [
+            'service' => $service,
+            'send' => $send,
+        ];
 
         $job = new SendTemplateMsgJob('wxapp', [], $template_id, $notice_data, $openid, '', '');
         $dispatch = dispatch($job);
