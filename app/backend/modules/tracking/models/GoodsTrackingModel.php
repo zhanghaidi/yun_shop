0b3879f35e936d3213fa7fc9bcad459acff53d8f
张海地 <398978233@qq.com>
2020-08-06 09:00:32 +0800

    增加商品追踪菜单和控制器页面显示

---
+++ app/backend/modules/tracking/models/GoodsTrackingModel.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: 芸众商城 www.yunzshop.com
+ * Date: 17/2/23
+ * Time: 下午10:57
+ */
+
+namespace app\backend\modules\tracking\modules;
+
+use Illuminate\Database\Eloquent\Model;
+
+class GoodsTrackingModel extends Model
+{
+    public $table = 'diagnostic_service_goods_tracking';
+
+
+}
