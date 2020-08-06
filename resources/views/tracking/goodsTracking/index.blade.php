d78d9d913fc965db2686b14de3aed9b42c6ebe1a
张海地 <398978233@qq.com>
2020-08-06 18:25:47 +0800

    提交显示状态

--- resources/views/tracking/goodsTracking/index.blade.php
+++ resources/views/tracking/goodsTracking/index.blade.php
@@ -40,9 +40,11 @@
                                 <td style="text-align: center;">{{ $list->resource_id }}</td>
                                 <td style="text-align: center;">{{ $list->goods->title }}</td>
                                 <td style="text-align: center;">
+                                    <a href="{{yzWebUrl('member.member.index',array('search%5Bmid%5D=' => $list->user_id ))}}">
                                         <img src='{{$list->user->avatarurl}}'
                                              style='width:30px;height:30px;padding:1px;border:1px solid #ccc'/><br/>
-                                    {{ $list->user->nickname }}
+                                        {{ $list->user->nickname }}
+                                    </a>
 
                                 </td>
                                 <td style="text-align: center;">{!! $list->action !!}</td>
