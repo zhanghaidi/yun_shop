27136f7269786dd06299bedec1379ed43d562f2b
张海地 <398978233@qq.com>
2020-08-10 10:05:41 +0800

    订单显示断点

--- app/backend/modules/tracking/controllers/GoodsTrackingController.php
+++ app/backend/modules/tracking/controllers/GoodsTrackingController.php
@@ -15,14 +15,8 @@ class GoodsTrackingController extends BaseController
     public function index(){
         $pageSize = 20;
         $list = GoodsTrackingModel::with(['goods','user','resource','order'])->paginate($pageSize);
-        //dd($list);
 
-        foreach ($list as $v){
-            var_dump($v->action_id);
-        }
-        dd($list);
         $pager = PaginationHelper::show($list['total'], $list['current_page'], $list['per_page']);
-
 
         return view('tracking.goodsTracking.index', [
             'pageList' => $list,
