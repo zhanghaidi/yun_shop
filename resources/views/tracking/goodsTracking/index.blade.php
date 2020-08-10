d8aa5add0906e435d87e54365ce43337b582124a
张海地 <398978233@qq.com>
2020-08-10 11:33:12 +0800

    埋点搜索功能页面

--- resources/views/tracking/goodsTracking/index.blade.php
+++ resources/views/tracking/goodsTracking/index.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.base')
-@section('title','商品追踪列表')
+@section('title','商品埋点列表')
 
 @section('content')
     <div id="member-blade" class="rightlist">
@@ -7,11 +7,11 @@
             <div class="panel-heading">
                 <span>当前位置：</span>
                 <a href="{{yzWebUrl('tracking.goods-tracking.index')}}">
-                    <span>商品追踪</span>
+                    <span>商品埋点</span>
                 </a>
                 <span>>></span>
                 <a href="#">
-                    <span>追踪列表</span>
+                    <span>埋点列表</span>
                 </a>
             </div>
 
@@ -21,50 +21,22 @@
                     <input type="hidden" name="c" value="site"/>
                     <input type="hidden" name="a" value="entry"/>
                     <input type="hidden" name="m" value="yun_shop"/>
-                    <input type="hidden" name="do" value="member" id="form_do"/>
-                    <input type="hidden" name="route" value="member.member.index" id="route"/>
-                    <div class="form-group col-xs-12 col-sm-2 col-md-2 col-lg-2 ">
-                        <!--<label class="col-xs-12 col-sm-2 col-md-2 col-lg-2 control-label">ID</label>-->
-                        <div class="">
-                            <input type="text" placeholder="会员ID" class="form-control" name="search[mid]"
-                                   value="{{$request['search']['mid']}}"/>
-                        </div>
-                    </div>
+                    <input type="hidden" name="do" value="tracking" id="form_do"/>
+                    <input type="hidden" name="route" value="tracking.goods-tracking.index" id="route"/>
+
                     <div class="form-group col-xs-12 col-sm-2 col-md-2 col-lg-2">
                         <!-- <label class="col-xs-12 col-sm-2 col-md-2 col-lg-2 control-label">会员信息</label>-->
                         <div class="">
                             <input type="text" class="form-control" name="search[realname]"
-                                   value="{{$request['search']['realname']}}" placeholder="可搜索昵称/姓名/手机号"/>
+                                   value="{{$request['search']['realname']}}" placeholder="可搜索会员ID/昵称/姓名/手机号"/>
                         </div>
                     </div>
-                    <div class="form-group col-xs-12 col-sm-2 col-md-2 col-lg-2">
+
+                    <div class="form-group col-xs-12 col-sm-2 col-md-2 col-lg-2 ">
+                        <!--<label class="col-xs-12 col-sm-2 col-md-2 col-lg-2 control-label">ID</label>-->
                         <div class="">
-                            <input type="text" class="form-control" name="search[first_count]"
-                                   value="{{$request['search']['first_count']}}" placeholder="一级人数"/>
-                        </div>
-                    </div>
-                    <div class="form-group col-xs-12 col-sm-2 col-md-2 col-lg-2">
-                        <div class="">
-                            <input type="text" class="form-control" name="search[second_count]"
-                                   value="{{$request['search']['second_count']}}" placeholder="二级人数"/>
-                        </div>
-                    </div>
-                    <div class="form-group col-xs-12 col-sm-2 col-md-2 col-lg-2">
-                        <div class="">
-                            <input type="text" class="form-control" name="search[third_count]"
-                                   value="{{$request['search']['third_count']}}" placeholder="三级人数"/>
-                        </div>
-                    </div>
-                    <div class="form-group col-xs-12 col-sm-2 col-md-2 col-lg-2">
-                        <div class="">
-                            <input type="text" class="form-control" name="search[team_count]"
-                                   value="{{$request['search']['team_count']}}" placeholder="团队人数"/>
-                        </div>
-                    </div>
-                    <div class="form-group col-xs-12 col-sm-2 col-md-2 col-lg-2">
-                        <div class="">
-                            <input type="text" class="form-control" name="search[custom_value]"
-                                   value="{{$request['search']['custom_value']}}" placeholder="自定义字段"/>
+                            <input type="text" placeholder="商品名/穴位名/" class="form-control" name="search[name]"
+                                   value="{{$request['search']['name']}}"/>
                         </div>
                     </div>
 
@@ -86,95 +58,80 @@
                     <div class="form-group col-xs-12 col-sm-2 col-md-2 col-lg-2">
                         <!--  <label class="col-xs-12 col-sm-2 col-md-2 col-lg-2 control-label">会员分组</label>-->
                         <div class="">
-                            <select name='search[groupid]' class='form-control'>
-                                <option value=''>会员分组不限</option>
-                                @foreach($groups as $group)
-                                    <option value='{{$group['id']}}'
-                                            @if($request['search']['groupid']==$group['id'])
-                                            selected
-                                            @endif
-                                    >{{$group['group_name']}}</option>
-                                @endforeach
-                            </select>
-                        </div>
-                    </div>
-
-
-                    <div class="form-group col-xs-12 col-sm-2 col-md-2 col-lg-2">
-                        <div class="">
-                            <select name='search[isagent]' class='form-control'>
-                                <option value=''>推广员不限</option>
-                                <option value='0'
-                                        @if($request['search']['isagent']=='0')
-                                        selected
-                                        @endif>否
-                                </option>
+                            <select name='search[type_id]' class='form-control'>
+                                <option value=''>来源类型不限</option>
                                 <option value='1'
-                                        @if($request['search']['isagent']=='1')
+                                        @if($request['search']['type_id']=='1')
                                         selected
-                                        @endif>是
+                                        @endif>穴位
+                                </option>
+                                <option value='3'
+                                        @if($request['search']['type_id']=='3')
+                                        selected
+                                        @endif>文章
+                                </option>
+                                <option value='4'
+                                        @if($request['search']['type_id']=='4')
+                                        selected
+                                        @endif>社区话题
+                                </option>
+                                <option value='5'
+                                        @if($request['search']['type_id']=='5')
+                                        selected
+                                        @endif>体质
+                                </option>
+                                <option value='6'
+                                        @if($request['search']['type_id']=='6')
+                                        selected
+                                        @endif>灸师
                                 </option>
                             </select>
                         </div>
                     </div>
+
+
                     <div class="form-group col-xs-12 col-sm-2 col-md-2 col-lg-2">
                         <!--      <label class="col-xs-12 col-sm-2 col-md-2 col-lg-2 control-label">是否关注</label>-->
                         <div class="">
-                            <select name='search[followed]' class='form-control'>
-                                <option value=''>不限关注</option>
+                            <select name='search[action_id]' class='form-control'>
+                                <option value=''>不限动作</option>
                                 </option>
                                 <option value='1'
-                                        @if($request['search']['followed']=='1')
+                                        @if($request['search']['action_id']=='1')
                                         selected
                                         @endif
-                                >已关注
+                                >查看
                                 </option>
-                                <option value='0'
-                                        @if($request['search']['followed']=='0')
+                                <option value='2'
+                                        @if($request['search']['action_id']=='2')
                                         selected
                                         @endif
-                                >未关注
+                                >收藏
                                 </option>
+                                <option value='3'
+                                        @if($request['search']['action_id']=='3')
+                                        selected
+                                        @endif
+                                >加购
+                                </option>
+
+                                <option value='4'
+                                        @if($request['search']['action_id']=='下单')
+                                        selected
+                                        @endif
+                                >下单
+                                </option>
+
+                                <option value='5'
+                                        @if($request['search']['action_id']=='支付')
+                                        selected
+                                        @endif
+                                >支付
+                                </option>
+
                             </select>
                         </div>
                     </div>
-                    <div class="form-group col-xs-12 col-sm-2 col-md-2 col-lg-2">
-                        <!--        <label class="col-xs-12 col-sm-2 col-md-2 col-lg-2 control-label">黑名单</label>-->
-                        <div class="">
-                            <select name='search[isblack]' class='form-control'>
-                                <option value=''>不限黑名单</option>
-                                <option value='0'
-                                        @if($request['search']['isblack']=='0')
-                                        selected
-                                        @endif>否
-                                </option>
-                                <option value='1'
-                                        @if($request['search']['isblack']=='1')
-                                        selected
-                                        @endif>是
-                                </option>
-                            </select>
-                        </div>
-                    </div>
-
-
-                    <div class="form-group col-xs-12 col-sm-12 col-md-4 col-lg">
-
-                        <div class="time">
-
-                            <select name='search[searchtime]' class='form-control'>
-                                <option value='0'
-                                        @if($request['search']['searchtime']=='0')
-                                        selected
-                                        @endif>注册时间不限
-                                </option>
-                                <option value='1'
-                                        @if($request['search']['searchtime']=='1')
-                                        selected
-                                        @endif>搜索注册时间
-                                </option>
-                            </select>
-                        </div>
                         <div class="search-select">
                             {!! app\common\helpers\DateRange::tplFormFieldDateRange('search[times]', [
                             'starttime'=>date('Y-m-d H:i', $starttime),
@@ -183,7 +140,6 @@
                             'end'=>0
                             ], true) !!}
                         </div>
-                    </div>
 
                     <div class="form-group  col-xs-12 col-md-12 col-lg-6">
                         <!--<label class="col-xs-12 col-sm-2 col-md-2 col-lg-2 control-label"></label>-->
@@ -201,7 +157,7 @@
 
             </div>
 
-            
+
         </div>
         <div class="clearfix">
             <div class="panel panel-default">
