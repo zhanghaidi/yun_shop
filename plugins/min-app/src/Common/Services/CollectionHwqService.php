4071b964b1a3b3a2c771c6b91ef4f4672d507fb4
zhd <398978233@qq.com>
2020-10-29 17:08:30 +0800

    芸众果果消息提醒和小程序插件access_token统一使用线上

--- plugins/min-app/src/Common/Services/CollectionHwqService.php
+++ plugins/min-app/src/Common/Services/CollectionHwqService.php
@@ -78,11 +78,16 @@ class CollectionHwqService
             return;
         }
 
-        $url =  'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='.$set['key'].'&secret='.$set['secret'];
+        //fixby-zhd-小程序access_token统一使用微擎生产线上api 2020-10-29
 
+        //$url =  'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='.$set['key'].'&secret='.$set['secret'];
+        //$result = Curl::to($url)->asJsonResponse(true)->get();
+        //return $result['access_token'];
+
+        $url =  'https://www.aijuyi.net/api/accesstoken.php?type=4&appid='.$set['shop_key'].'&secret='.$set['shop_secret'];
         $result = Curl::to($url)->asJsonResponse(true)->get();
 
-        return $result['access_token'];
+        return $result['accesstoken'];
     }
 
     /**
