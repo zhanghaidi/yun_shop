f00b306feab862cc14deafa173a6bada94d8d0ed
zhd <398978233@qq.com>
2020-11-14 16:59:16 +0800

    发送消息队列日志

--- app/Jobs/TemplateMsgSendWechtJob.php
+++ app/Jobs/TemplateMsgSendWechtJob.php
@@ -63,20 +63,25 @@ class TemplateMsgSendWechtJob implements ShouldQueue
         //$ids = [125519,114685,129411,129419,125310, 129415, 114545]; ->whereIn('uid',$ids)
             //查询公众号粉丝 发送模板消息
         $weid = intval($this->config['weid']);
+        $i= 0;
             DB::table('mc_mapping_fans')->where(['uniacid' => $weid, 'follow' => 1])
-                ->chunk(1000, function ($mapping_fans_list) {
+                ->chunk(1000, function ($mapping_fans_list) use ($i) {
                     foreach ($mapping_fans_list as $mapping_fans) {
                         /*$job = new SendTemplateMsgJob($this->config['type'], $this->config['options'], $this->config['template_id'], $this->config['notice_data'],
                             $mapping_fans['openid'], '', $this->config['page']);
                         dispatch($job);*/
+
                         Log::info($mapping_fans['uniacid'].' :fanid:'.$mapping_fans['fanid'].'--- uid:'.$mapping_fans['uid']);
+
+                        $i++;
                     }
 
                 });
 
 
         $end = time();
+        $totalSecond = $end-$begin;
         Log::info('TemplateMsgSendWechtJob队列执行完毕'.date('Y-m-d H:i:s',$end));
-        Log::info('TemplateMsgSendWechtJob队列执行时间：'.$end-$begin);
+        Log::info('TemplateMsgSendWechtJob队列执行共'.$i.'条,时间花费了：'.$totalSecond.'秒');
     }
 }
