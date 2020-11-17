962b41ba6c4c94b88030a9560d5b023765c1bb65
zhd <398978233@qq.com>
2020-11-17 15:56:15 +0800

    一键发送消息队列通过接受获取

--- app/Jobs/TemplateMsgSendWechtJob.php
+++ app/Jobs/TemplateMsgSendWechtJob.php
@@ -65,9 +65,9 @@ class TemplateMsgSendWechtJob implements ShouldQueue
         $openid_arr = $this->config['openid'];
         Log::info('openid:' .json_encode($openid_arr));
         foreach ($openid_arr as $k => $openid){
-            //$job = new SendTemplateMsgJob($this->config['type'], $this->config['options'], $this->config['template_id'], $this->config['notice_data'], $openid, '', $this->config['page']);
-            //dispatch($job);
-            Log::info($weid.': $k -'.$openid);
+            $job = new SendTemplateMsgJob($this->config['type'], $this->config['options'], $this->config['template_id'], $this->config['notice_data'], $openid, '', $this->config['page']);
+            dispatch($job);
+            Log::info($k.' : '.$weid.' '.$openid);
         }
         /*DB::table('mc_mapping_fans')->whereIn('openid', $this->config['openid'])->where(['uniacid' => $weid, 'follow' => 1])
             ->chunk(1000, function ($mapping_fans_list) {
