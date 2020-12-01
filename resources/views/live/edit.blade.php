9c703278fa692b8e212d90f931bac49b3391b169
张李涛 <215467829@qq.com>
2020-12-01 11:56:29 +0800

    新增关联商品

--- resources/views/live/edit.blade.php
+++ resources/views/live/edit.blade.php
@@ -56,6 +56,53 @@
                         </div>
 
                         <div class="form-group">
+                            <label class="col-xs-12 col-sm-3 col-md-2 control-label">关联商品</label>
+                            <div class="col-sm-9 col-xs-12">
+                                <table class="table">
+                                    <tbody id="param-itemsgoods">
+                                    @if ($live['goods_ids'])
+                                        @foreach ($live->goods() as $k=>$v)
+                                            <tr>
+                                                <td style="text-align: center;width: 40px">
+                                                    <a href="javascript:;" onclick="deleteParam(this)" style="margin-top:10px;"  title="删除"><i class='fa fa-times'></i></a>
+                                                </td>
+                                                <td  colspan="2">
+                                                    <input type="hidden" class="form-control" name="live[goods_ids][]" data-id="{{$v['id']}}" data-name="goodsids"  value="{{$v['id']}}" style="width:200px;float:left"  />
+                                                    <input class="form-control" type="text" name="live[goods_names][]" data-id="{{$v['id']}}" data-name="goodsnames" value="{{$v['title']}}" style="width:400px;float:left" readonly="true">
+                                                    <span class="input-group-btn">
+                                                        <button class="btn btn-default nav-link-goods" type="button" data-id="{{$v['id']}}" onclick="$('#modal-module-menus-goods').modal();$(this).parent().parent().addClass('focusgood')">选择商品</button>
+                                                    </span>
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                    @else
+                                        <tr>
+                                            <td style="text-align: center;width: 40px">
+                                                <a href="javascript:;" onclick="deleteParam(this)" style="margin-top:10px;"  title="删除"><i class='fa fa-times'></i></a>
+                                            </td>
+                                            <td  colspan="2">
+                                                <input type="hidden" class="form-control" name="live[goods_ids][]" data-id="" data-name="goodsids"  value="" style="width:200px;float:left"  />
+                                                <input class="form-control" type="text" name="live[goods_names][]" data-id="" data-name="goodsnames" value="" style="width:400px;float:left" readonly="true">
+                                                <span class="input-group-btn">
+                                                    <button class="btn btn-default nav-link-goods" type="button" data-id="" onclick="$('#modal-module-menus-goods').modal();$(this).parent().parent().addClass('focusgood')">选择商品</button>
+                                                </span>
+                                            </td>
+                                        </tr>
+                                    @endif
+                                    </tbody>
+
+                                    <tbody>
+                                    <tr>
+                                        <td colspan="3">
+                                            <a href="javascript:;" id="add-param_goods" onclick="addParam('goods' )" style="margin-top:10px;" class="btn btn-primary" title="添加商品"><i class="fa fa-plus"></i> 添加商品</a>
+                                        </td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
                             <label class="col-xs-12 col-sm-3 col-md-2 control-label"><span >*</span>分享标题</label>
                             <div class="col-sm-9 col-xs-12"><input type="text" name="live[share_title]" class="form-control" value="{{$live['share_title']}}" placeholder="请输入分享标题"/></div>
                         </div>
@@ -120,10 +167,67 @@
                     </div>
                 </div>
             </form>
+
+            <div id="modal-module-menus-goods" class="modal fade" tabindex="-1"> {{--搜索商品的弹窗--}}
+                <div class="modal-dialog" style='width: 920px;'>
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button aria-hidden="true" data-dismiss="modal" class="close" type="button">×</button>
+                            <h3>选择商品</h3>
+                        </div>
+                        <div class="modal-body">
+                            <div class="row">
+                                <div class="input-group">
+                                    <input type="text" class="form-control" name="keyword" value="" id="search-kwd-goods" placeholder="请输入商品名称"/>
+                                    <span class='input-group-btn'>
+                                        <button type="button" class="btn btn-default" onclick="search_goods();">搜索</button>
+                                    </span>
+                                </div>
+                            </div>
+                            <div id="module-menus-goods" style="padding-top:5px;"></div>
+                        </div>
+                        <div class="modal-footer">
+                            <a href="#" class="btn btn-default" data-dismiss="modal" aria-hidden="true">关闭</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 
     <script>
+        function addParam(type) {
+            var data = '<tr><td style="text-align: center;width: 40px"><a href="javascript:;" onclick="deleteParam(this)" style="margin-top:10px;" title="删除"><i class=\'fa fa-times\'></i></a></td><td colspan="2"><input type="hidden" class="form-control" name="live[goods_ids][]"  data-id="{$id}" data-name="goodsids" value="" placeholder="按钮名称" style="width:200px;float:left"  /><input class="form-control" type="text" data-id="{$id}" data-name="goodsnames" placeholder="" value="" name="live[goods_names][]" style="width:400px;float:left" readonly="true"><span class="input-group-btn"><button class="btn btn-default nav-link-goods" type="button" data-id="{$id}" onclick="$(\'#modal-module-menus-goods\').modal();$(this).parent().parent().addClass(\'focusgood\')">选择商品</button></span></td></tr>';
+            $('#param-items'+type).append(data);
+        }
 
+        function deleteParam(o) {
+            $(o).parent().parent().remove();
+        }
+        function saveadd(o) {
+            $(o).parent().parent().remove();
+        }
+
+        {{--搜索商品--}}
+        function search_goods() {
+            if ($.trim($('#search-kwd-goods').val()) == '') {
+                Tip.focus('#search-kwd-goods', '请输入关键词');
+                return;
+            }
+            $("#module-menus-goods").html("正在搜索....");
+            $.get('{!! yzWebUrl('goods.goods.get-search-goods') !!}', {
+                    keyword: $.trim($('#search-kwd-goods').val())
+                }, function (dat) {
+                    $('#module-menus-goods').html(dat);
+                }
+            );
+        }
+        function select_good(o) {
+            console.log(o);
+            $(".focusgood:last input[data-name=goodsids]").val(o.id);
+            $(".focusgood:last input[data-name=goodsnames]").val(o.title);
+            $(".focusgood").removeClass("focusgood");
+            $("#modal-module-menus-goods .close").click();
+        }
     </script>
 @endsection
