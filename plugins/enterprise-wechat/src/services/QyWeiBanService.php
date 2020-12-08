a9929f68152c892374be4d98107c7f48a132b711
zhd <398978233@qq.com>
2020-12-08 14:13:09 +0800

    微伴助手订单批量上报调整

--- plugins/enterprise-wechat/src/services/QyWeiBanService.php
+++ plugins/enterprise-wechat/src/services/QyWeiBanService.php
@@ -22,32 +22,32 @@ class QyWeiBanService
     {
 
         $set = \Setting::get('plugin.enterprise-wechat');
-        if(!$set){
-            \Log::info('企业微信配置',$set);
+        if (!$set) {
+            \Log::info('企业微信配置', $set);
             return false;
 
         }
-        if(!$set['weiban_corpid']){
-            \Log::info('企业微信微伴企业corpid不存在',$set);
+        if (!$set['weiban_corpid']) {
+            \Log::info('企业微信微伴企业corpid不存在', $set);
             return false;
         }
-        if(!$set['weiban_secret']){
-            \Log::info('企业微信微伴Secret不存在',$set);
+        if (!$set['weiban_secret']) {
+            \Log::info('企业微信微伴Secret不存在', $set);
             return false;
         }
 
 
         //企业微伴access_token获取地址
-        $url =  'https://open.weibanzhushou.com/open-api/access_token/get';
+        $url = 'https://open.weibanzhushou.com/open-api/access_token/get';
         $data = array(
             'corp_id' => $set['weiban_corpid'],
             'secret' => $set['weiban_secret']
         );
 
         $result = Curl::to($url)->withData(json_encode($data))->withContentType('application/json')->asJsonResponse(true)->post();
-        if($result['errcode']!= 0){
+        if ($result['errcode'] != 0) {
             return false;
-            \Log::info('企业微信微伴token获取失败',$result);
+            \Log::info('企业微信微伴token获取失败', $result);
         }
         return $result['access_token'];
     }
@@ -56,12 +56,12 @@ class QyWeiBanService
     public static function importOrder($order)
     {
 
-        if(!$order){
+        if (!$order) {
             throw new AppException("订单不能为空");
         }
 
         $accessToken = self::getAccessToken();
-        if(!$accessToken){
+        if (!$accessToken) {
             throw new AppException("access_token调用失败");
         }
 
@@ -69,9 +69,9 @@ class QyWeiBanService
         $url = "https://open.weibanzhushou.com/open-api/order/import_order?access_token={$accessToken}";
 
         $response = Curl::to($url)->withData(json_encode($order))->withContentType('application/json')->asJsonResponse(true)->post();
-        if($response['errcode']!= 0){
+        if ($response['errcode'] != 0) {
             throw new AppException("订单上报失败", $response['errmsge']);
-            \Log::info('企业微信微伴订单上报失败',$response);
+            \Log::info('企业微信微伴订单上报失败', $response);
         }
 
         return $response;
@@ -82,15 +82,15 @@ class QyWeiBanService
     public static function getOrderList()
     {
         $accessToken = self::getAccessToken();
-        if(!$accessToken){
+        if (!$accessToken) {
             throw new AppException("access_token调用失败");
         }
         //获取订单列表数据
         $url = "https://open.weibanzhushou.com/open-api/order/list?access_token={$accessToken}";
         $response = Curl::to($url)->asJsonResponse(true)->get();
-        if($response['errcode']!= 0){
+        if ($response['errcode'] != 0) {
             throw new AppException("订单获取失败", $response['errmsge']);
-            \Log::info('企业微信微伴订单获取失败',$response);
+            \Log::info('企业微信微伴订单获取失败', $response);
         }
 
         return $response;
@@ -99,23 +99,52 @@ class QyWeiBanService
     //移除订单
     public static function removeOrder($order_id)
     {
-        if(!$order_id){
+        if (!$order_id) {
             throw new AppException("订单编号不能为空");
         }
         $accessToken = self::getAccessToken();
-        if(!$accessToken){
+        if (!$accessToken) {
             throw new AppException("access_token调用失败");
         }
 
         //移除订单 https://open.weibanzhushou.com/open-api/order/remove
         $url = "https://open.weibanzhushou.com/open-api/order/remove?access_token={$accessToken}&order_id={$order_id}";
         $response = Curl::to($url)->asJsonResponse(true)->get();
-        if($response['errcode']!= 0){
+        if ($response['errcode'] != 0) {
             throw new AppException("订单删除失败", $response['errmsge']);
-            \Log::info('企业微信微伴订单删除失败',$response);
+            \Log::info('企业微信微伴订单删除失败', $response);
         }
 
         return $response;
+    }
+
+    //订单信息批量导入
+    public static function sendOrderList($orderList)
+    {
+
+        if (empty($orderList)) {
+            throw new AppException("订单列表不能为空");
+        }
+        if (count($orderList) > 100) {
+            throw new AppException("最多支持传入100个订单");
+        }
+
+        $accessToken = self::getAccessToken();
+        if (!$accessToken) {
+            throw new AppException("access_token调用失败");
+        }
+
+        //批量订单信息同步接口https://open.weibanzhushou.com/open-api/order/batch_import_order
+        $url = "https://open.weibanzhushou.com/open-api/order/batch_import_order?access_token={$accessToken}";
+
+        $response = Curl::to($url)->withData(json_encode($orderList))->withContentType('application/json')->asJsonResponse(true)->post();
+        if ($response['errcode'] != 0) {
+            throw new AppException("批量订单上报失败", $response['errmsge']);
+            \Log::info('企业微信微伴订单批量上报失败', $response);
+        }
+
+        return $response;
+
     }
 
 
