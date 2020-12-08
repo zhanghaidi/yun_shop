70f9ba334f18f0daf57b55b298741ff349df74ef
zhd <398978233@qq.com>
2020-12-08 13:41:19 +0800

    微伴助手订单删除测试

--- plugins/enterprise-wechat/src/services/QyWeiBanService.php
+++ plugins/enterprise-wechat/src/services/QyWeiBanService.php
@@ -13,11 +13,6 @@ use app\common\exceptions\AppException;
 
 class QyWeiBanService
 {
-    private $order;
-    private $accessToken;
-    public function __construct(){
-
-    }
 
     /**
      * 获取微伴助手access_token
@@ -57,8 +52,9 @@ class QyWeiBanService
         return $result['access_token'];
     }
 
-    //订单同步
-    public static function importOrder($order){
+    //单订单导入
+    public static function importOrder($order)
+    {
 
         if(!$order){
             throw new AppException("订单不能为空");
@@ -83,7 +79,8 @@ class QyWeiBanService
     }
 
     //获取用户订单
-    public static function getOrderList(){
+    public static function getOrderList()
+    {
         $accessToken = self::getAccessToken();
         if(!$accessToken){
             throw new AppException("access_token调用失败");
@@ -94,6 +91,28 @@ class QyWeiBanService
         if($response['errcode']!= 0){
             throw new AppException("订单获取失败", $response['errmsge']);
             \Log::info('企业微信微伴订单获取失败',$response);
+        }
+
+        return $response;
+    }
+
+    //移除订单
+    public static function removeOrder($order_id)
+    {
+        if(!$order_id){
+            throw new AppException("订单编号不能为空");
+        }
+        $accessToken = self::getAccessToken();
+        if(!$accessToken){
+            throw new AppException("access_token调用失败");
+        }
+
+        //移除订单 https://open.weibanzhushou.com/open-api/order/remove
+        $url = "https://open.weibanzhushou.com/open-api/order/remove?access_token={$accessToken}&order_id={$order_id}";
+        $response = Curl::to($url)->asJsonResponse(true)->get();
+        if($response['errcode']!= 0){
+            throw new AppException("订单删除失败", $response['errmsge']);
+            \Log::info('企业微信微伴订单删除失败',$response);
         }
 
         return $response;
