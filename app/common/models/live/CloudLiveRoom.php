91b60607034580e32ab1dd806c1b8715e76fc924
zhd <398978233@qq.com>
2020-12-17 10:33:45 +0800

    关联直播间点赞

--- app/common/models/live/CloudLiveRoom.php
+++ app/common/models/live/CloudLiveRoom.php
@@ -44,7 +44,7 @@ class CloudLiveRoom extends BaseModel
     protected $guarded = [''];
     protected $mediaFields = ['cover_img', 'share_img', 'header_img'];
     protected $casts = ['start_time' => 'date', 'end_time' => 'date', 'updated_at' => 'date', 'created_at' => 'date'];
-    protected $appends = ['status_parse','is_subscription','like_num','subscription_num'];
+    protected $appends = ['status_parse'];
 
     protected static $liveStatus = [0 => '关闭', 101 => '直播中', 102 => '未开始', 103 => '已结束', 104 => '禁播', 105 => '暂停', 106 => '异常', 107 => '已过期'];
 
