73c7cfb54fb7b784c57b1783b933fd0901de0d1c
zhd <398978233@qq.com>
2020-12-23 15:23:54 +0800

    直播间时间title

--- app/frontend/controllers/TestController.php
+++ app/frontend/controllers/TestController.php
@@ -72,8 +72,8 @@ class TestController extends BaseController
 
     public function testlive(){
         $time_now = time();
-        $wait_seconds = 7200 * 100;
-        $check_time_range = [$time_now, $time_now + $wait_seconds];
+        $wait_seconds = 60 * 1;
+        $check_time_range = [$time_now - $wait_seconds - 60,$time_now - $wait_seconds];
 
         // 1、查询开始时间距离当前时间2分钟之内开播的直播 where('live_status', 101)暂时不卡播放状态 where('start_time', $check_time_range)->
         $startLiveRoom = CloudLiveRoom::whereBetween('start_time', $check_time_range)->select('id','name','live_status','start_time','anchor_name')->with('hasManySubscription')->get()->toArray();
