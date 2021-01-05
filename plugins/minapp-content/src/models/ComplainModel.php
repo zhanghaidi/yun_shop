63ae223fc067fbae4d1bd9868c790c320ba1d7a4
wangkai <1170824885@qq.comm>
2021-01-05 17:51:25 +0800

    用户反馈，发票，经络列表，穴位排序接口迁移

---
+++ plugins/minapp-content/src/models/ComplainModel.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Yunshop\MinappContent\models;
+
+use app\common\models\BaseModel;
+
+/**
+ * Class ComplainModel
+ * @package Yunshop\MinappContent\models
+ * 用户投诉模型
+ */
+class ComplainModel extends BaseModel
+{
+    public $table = 'diagnostic_service_complain';
+}
