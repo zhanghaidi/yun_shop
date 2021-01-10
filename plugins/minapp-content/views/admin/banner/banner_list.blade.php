a51f843f665c7e97f0754a8321deaab2ab4a3318
wangkai <1170824885@qq.comm>
2021-01-10 18:00:41 +0800

    banner图功能

--- plugins/minapp-content/views/admin/banner/banner_list.blade.php
+++ plugins/minapp-content/views/admin/banner/banner_list.blade.php
@@ -9,46 +9,17 @@
 
             <div class="top" style="margin-bottom:20px">
                 <ul class="add-shopnav" id="myTab">
-                    <li @if($type=='step') class="active" @endif><a
+                    <li @if($type=='banner') class="active" @endif><a
                                 href="{{yzWebUrl('plugin.minapp-content.admin.sport-clock.step')}}">轮播图列表</a></li>
-                    <li @if($type=='step_exchange_list') class="active" @endif><a
+                    <li @if($type=='banner_position') class="active" @endif><a
                                 href="{{yzWebUrl('plugin.minapp-content.admin.sport-clock.step-exchange-list')}}">轮播位</a>
                     </li>
                 </ul>
             </div>
 
             <div class="panel-body">
-                <form id="form1" role="form" class="form-horizontal form" method="post" action="">
-                    <div class="form-group">
-                        <div class="col-xs-12 col-sm-3 col-md-3 col-lg-2">
-                            <select name='search[status]' class='form-control'>
-                                <option value='0' @if($request['search']['status']==0) selected @endif>选择快评类型
-                                </option>
-                                <option value='1' @if($request['search']['status']==1) selected @endif>穴位快评
-                                </option>
-                                <option value='3' @if($request['search']['status']==3) selected @endif>文章快评
-                                </option>
-                                <option value='4' @if($request['search']['status']==4) selected @endif>帖子快评
-                                </option>
-                                <option value='5' @if($request['search']['status']==5) selected @endif>课程快评
-                                </option>
-                                <option value='6' @if($request['search']['status']==6) selected @endif>灸师快评
-                                </option>
-                                <option value='7' @if($request['search']['status']==7) selected @endif>直播快捷消息
-                                </option>
-                            </select>
-                        </div>
-                        <div class="col-xs-12 col-sm-3 col-md-3 col-lg-2">
-                            <input type="text" class="form-control" name="search[content]"
-                                   value="{{$request['search']['content']}}" placeholder="请输入快评内容"/>
-                        </div>
-                        <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
-                            <button type="submit" class="btn btn-success"><i class="fa fa-search"></i>搜索</button>
-                        </div>
-                    </div>
-                </form>
                 <div class="col-xs-12 col-sm-2 col-md-2 col-lg-2">
-                    <a href="{{ yzWebUrl('plugin.minapp-content.admin.quick-comment.edit') }}" class="btn btn-info">添加快捷评语</a>
+                    <a href="{{ yzWebUrl('plugin.minapp-content.admin.banner.add') }}" class="btn btn-info">添加轮播图</a>
                 </div>
             </div>
 
@@ -57,53 +28,71 @@
                     <table class="table">
                         <thead>
                         <tr>
-                            <th>ID</th>
-                            <th>快评类型</th>
-                            <th>快评内容</th>
-                            <th>是否开启</th>
+                            <th>排序</th>
+                            <th>轮播图位置</th>
+                            <th>轮播图标题</th>
+                            <th>轮播图</th>
+                            <th>是否外链</th>
+                            <th>跳转地址</th>
+                            <th>跳转类型</th>
+                            <th>显示状态</th>
+                            <th>模板类型</th>
                             <th>添加时间</th>
                             <th>操作</th>
                         </tr>
                         </thead>
                         <tbody>
-                        @foreach($comments as $k => $value)
+                        @foreach($banner as $k => $value)
                             <tr>
-                                <td>{{$value['id']}}</td>
+                                <td>{{$value['list_order']}}</td>
+                                <td>{{$value['name']}}</td>
+                                <td>{{$value['title']}}</td>
                                 <td>
-                                    @if ($value['type'] == 1)
-                                        <span class="btn btn-white"> 穴位快评</span>
-                                    @elseif ($value['type'] == 2)
-                                        <span class="btn btn-white"> -- </span>
-                                    @elseif ($value['type'] == 3)
-                                        <span class="btn btn-white"> 文章快评</span>
-                                    @elseif ($value['type'] == 4)
-                                        <span class="btn btn-white"> 帖子快评</span>
-                                    @elseif ($value['type'] == 5)
-                                        <span class="btn btn-white"> 课程快评</span>
-                                    @elseif ($value['type'] == 6)
-                                        <span class="btn btn-white"> 灸师快评</span>
-                                    @elseif ($value['type'] == 7)
-                                        <span class="btn btn-white"> 直播快捷消息</span>
+                                    <a href="{{ tomedia($value['image']) }}" target="_blank">
+                                        <img src="{{tomedia($value['image'])}}" width="60">
+                                    </a>
+                                </td>
+                                <td>
+                                    @if ($value['is_href'] == 0)
+                                        否
+                                    @elseif ($value['type'] == 1)
+                                        是
+                                    @else
+                                        未知
+                                    @endif
+                                </td>
+                                <td>{{ $value['jumpurl'] }}</td>
+                                <td>
+                                    @if ($value['jumptype'] == 1)
+                                        普通页面
+                                    @elseif ($value['jumptype'] == 2)
+                                        底部导航
                                     @else
                                         未知
                                     @endif
                                 </td>
                                 <td>
-                                    {{ $value['content'] }}
-                                </td>
-                                <td>
                                     <a class="update-status"
-                                       href="{{ yzWebUrl('plugin.minapp-content.admin.quick-comment.display', ['id' => $value['id']]) }}"
+                                       href="{{ yzWebUrl('plugin.minapp-content.admin.banner.display', ['id' => $value['id']]) }}"
                                        data-status="{{ $value['status'] }}">
                                         @if ($value['status'] == 1)
-                                            <span class="label label-primary">开启</span>
+                                            <span class="label label-primary">显示</span>
                                         @else
-                                            <span class="label label-default">关闭</span>
+                                            <span class="label label-default">隐藏</span>
                                         @endif
                                     </a>
                                 </td>
                                 <td>
-                                    {{ date('Y-m-d H:i:s', $value['create_time']) }}
+                                    @if ($value['type'] == 1)
+                                        banner轮播图
+                                    @elseif ($value['type'] == 2)
+                                        功能区导航
+                                    @else
+                                        未知
+                                    @endif
+                                </td>
+                                <td>
+                                    {{ date('Y-m-d H:i:s', $value['add_time']) }}
                                 </td>
                                 <td>
                                     <a href="{{ yzWebUrl('plugin.minapp-content.admin.quick-comment.edit', ['id' => $value['id']]) }}"
