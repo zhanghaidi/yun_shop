569005032fd35a53d150c78d6870a15424338e19
zhd <398978233@qq.com>
2021-01-11 11:12:18 +0800

    二维码管理前端接口测试

--- plugins/activity-qrcode/src/api/QrcodeController.php
+++ plugins/activity-qrcode/src/api/QrcodeController.php
@@ -5,6 +5,9 @@ namespace Yunshop\ActivityQrcode\api;
 use app\common\components\ApiController;
 use app\common\facades\Setting;
 use Illuminate\Support\Facades\DB;
+use Yunshop\ActivityQrcode\models\Activity;
+use Yunshop\ActivityQrcode\models\Qrcode;
+use Yunshop\ActivityQrcode\models\ActivityUser;
 
 /**
  * Author: 芸众商城 www.yunzshop.com
@@ -20,7 +23,15 @@ class QrcodeController extends ApiController
     //活码维码展示页面
     public function index()
     {
-        return $this->successJson('ok-index');
+
+        $activityId =  \YunShop::request()->id;
+
+        $activityModel = Activity::getActivity($activityId);
+        if(!$activityModel){
+            return $this->message('无此记录或已被删除','','error');
+        }
+
+        return $this->successJson('ok', $activityModel);
 
     }
 
