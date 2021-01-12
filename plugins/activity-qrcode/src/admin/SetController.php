773d74a0c9216d9e0be870e1a1c0be41cf80bb94
zhd <398978233@qq.com>
2021-01-12 10:57:40 +0800

    二维码管理前端接口

--- plugins/activity-qrcode/src/admin/SetController.php
+++ plugins/activity-qrcode/src/admin/SetController.php
@@ -12,23 +12,24 @@ class SetController extends BaseController
 
     public function index()
     {
-        $set = Setting::get('plugin.activity-qrcode');
-        $requestModel = \YunShop::request()->setdata;
 
-        if ($requestModel) {
-            if (Setting::set('plugin.activity-qrcode', $requestModel)) {
-                return $this->message('设置成功', Url::absoluteWeb('plugin.activity-qrcode.admin.set'));
-            } else {
-                $this->error('设置失败');
+        $setting = Setting::get('plugin.activity-qrcode');
+        if (\Request::getMethod() == 'POST') {
+            $data = \YunShop::request()->setting;
+            if($data){
+                if (\Setting::set('plugin.activity-qrcode', $data)) {
+                    return $this->message('设置成功', Url::absoluteWeb('plugin.activity-qrcode.admin.set.index'));
+                } else {
+                    return $this->error('设置失败');
+                }
             }
         }
-
-        return view('Yunshop\ActivityQrcode::admin.set',
-            [
-                'setdata' => $set,
-            ]
-        )->render();
+        return view('Yunshop\ActivityQrcode::admin.set', [
+            'setting' => $setting
+        ])->render();
     }
+
+
 
 
 }
