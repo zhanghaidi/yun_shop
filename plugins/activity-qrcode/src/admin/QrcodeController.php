2facbe6da6e19e69885895d614dbb707b241b3b2
zhd <398978233@qq.com>
2021-01-12 15:46:59 +0800

    活码二维码插件

---
+++ plugins/activity-qrcode/src/admin/QrcodeController.php
@@ -0,0 +1,157 @@
+<?php
+
+namespace Yunshop\ActivityQrcode\admin;
+
+use app\common\components\BaseController;
+use app\common\facades\Setting;
+use app\common\helpers\Url;
+use app\common\helpers\PaginationHelper;
+use Yunshop\ActivityQrcode\models\Activity;
+use Yunshop\ActivityQrcode\models\Qrcode;
+use Yunshop\ActivityQrcode\services\ActivityQrcodeService;
+
+class QrcodeController extends BaseController
+{
+
+    protected $activityId;
+
+    public function __construct()
+    {
+        $this->activityId =  \YunShop::request()->id;
+        $activityModel = Activity::getActivity($this->activityId);
+        if(!$activityModel){
+
+            return $this->message('无此活码记录或已被删除','','error');
+        }
+
+    }
+
+    //首页
+    public function index()
+    {
+
+        $records = Qrcode::records();
+        $search = \YunShop::request()->search;
+        if ($search) {
+
+            $records = $records->search($search);
+
+        }
+
+        $recordList = $records->where('code_id', $this->activityId)->orderBy('sort', 'asc')->paginate();
+
+        $pager = PaginationHelper::show($recordList->total(), $recordList->currentPage(), $recordList->perPage());
+
+
+        return view('Yunshop\ActivityQrcode::admin.qrcodeList',
+            [
+                'activityId' => $this->activityId,
+                'pageList'    => $recordList,
+                'page'          => $pager,
+                'search'        => $search
+            ]
+        )->render();
+
+    }
+
+    //新增
+    public function add()
+    {
+        $qrList = Qrcode::where('code_id', $this->activityId)->get()->toArray();
+        $sortArr = array_column($qrList, 'sort');
+
+        $qrcodeModel = new Qrcode();
+        $requestQrcode = \YunShop::request()->info;
+        if ($requestQrcode) {
+            $requestQrcode['end_time'] = strtotime($requestQrcode['end_time']);
+
+            if(in_array($requestQrcode['sort'], $sortArr)){
+                return $this->message('排序已存在','','error');
+            }
+
+            $qrcodeModel->fill($requestQrcode);
+            $qrcodeModel->uniacid = \YunShop::app()->uniacid;
+            $qrcodeModel->code_id = $this->activityId;
+            //解析二维码内容
+            //$qrcodeModel->qr_path = ActivityQrcodeService::parseQrCode($qrcodeModel->qr_img);
+            //var_dump($qrcodeModel->qr_path);die;
+
+            $validator = $qrcodeModel->validator();
+            if($validator->fails()){
+                $this->error($validator->messages());
+            }else{
+                if($qrcodeModel->save()){
+                    return $this->message('添加成功', Url::absoluteWeb('plugin.activity-qrcode.admin.qrcode.index', array('id' => $this->activityId)));
+                }else{
+                    return $this->message('添加失败','','error');
+                }
+            }
+        }
+        return view('Yunshop\ActivityQrcode::admin.qrcodeInfo',
+            [
+                'info' => $qrcodeModel,
+            ]
+        )->render();
+
+    }
+
+
+    //编辑
+    public function edit()
+    {
+
+        $qrcodeId = \YunShop::request()->qrcode_id;
+        $qrList = Qrcode::where('code_id', $this->activityId)->where('id', '<>', $qrcodeId)->get()->toArray();
+        $sortArr = array_column($qrList, 'sort');
+
+        $qrcodeModel = Qrcode::getInfo($qrcodeId);
+        if(!$qrcodeModel){
+            return $this->message('二维码不存在或已被删除','','error');
+        }
+        $requestQrcode = \YunShop::request()->info;
+        if ($requestQrcode) {
+            $requestQrcode['end_time'] = strtotime($requestQrcode['end_time']);
+
+            if(in_array($requestQrcode['sort'], $sortArr)){
+                return $this->message('排序已存在','','error');
+            }
+
+            $qrcodeModel->fill($requestQrcode);
+
+            $validator = $qrcodeModel->validator();
+            if ($validator->fails()) {
+                $this->error($validator->messages());
+            } else {
+                if ($qrcodeModel->save()) {
+                    return $this->message('修改成功', Url::absoluteWeb('plugin.activity-qrcode.admin.qrcode.index', array('id' => $this->activityId)));
+                } else {
+                    return $this->message('修改失败','','error');
+                }
+            }
+        }
+        return view('Yunshop\ActivityQrcode::admin.qrcodeInfo',
+            [
+                'info' => $qrcodeModel,
+            ]
+        )->render();
+
+    }
+
+
+    //删除活码
+    public function deleted()
+    {
+        $qrcodeId = \YunShop::request()->qrcode_id;
+
+        if (!Qrcode::getInfo($qrcodeId)) {
+            return $this->message('没有此二维码或已被删除','','error');
+        }
+
+        if(Qrcode::deletedQrcode($qrcodeId)){
+            return $this->message('删除成功', Url::absoluteWeb('plugin.activity-qrcode.admin.qrcode.index',  array('id' => $this->activityId)));
+        }
+
+    }
+
+
+}
---
+++ plugins/activity-qrcode/src/api/QrcodeController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace Yunshop\ActivityQrcode\api;
+
+use app\common\components\ApiController;
+use app\common\facades\Setting;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Redis;
+use Yunshop\ActivityQrcode\models\Activity;
+use Yunshop\ActivityQrcode\models\Qrcode;
+use Yunshop\ActivityQrcode\models\ActivityUser;
+
+/**
+ * Author: 芸众商城 www.yunzshop.com
+ * Date: 2017/6/16
+ * Time: 下午2:29
+ */
+class QrcodeController extends ApiController
+{
+    protected $publicController = ['Qrcode'];
+    protected $publicAction = ['index', 'scan'];
+    protected $ignoreAction = ['index', 'scan'];
+
+    protected $activityId;
+
+    protected $ip;
+
+    public function __construct()
+    {
+        global $_W;
+
+        $activityId =  intval(\YunShop::request()->id);
+        if(!$activityId){
+            return $this->errorJson('参数错误', [
+                'status' => 0
+            ]);
+        }
+        $this->activityId = $activityId;
+
+        //搜集新加入此页面的用户
+        $this->userJoin($activityId, $_W);
+    }
+
+    //活码维码展示页面
+    public function index()
+    {
+
+        $activityModel = Activity::getActivity($this->activityId);
+        if(!$activityModel){
+            return $this->errorJson('活码不存在或已失效');
+        }
+
+        return $this->successJson('ok', $activityModel);
+
+    }
+
+   //扫码识别页面
+    public function scan()
+    {
+        global $_W;
+        $qrcode_id = intval(\YunShop::request()->qrcode_id);
+        if(!$qrcode_id){
+            return $this->errorJson('缺少参数');
+        }
+        $params = array(
+            'uniacid' => \YunShop::app()->uniacid,
+            'code_id' => $this->activityId,
+            'ip' => $_W['clientip'],
+        );
+
+        ActivityUser::where($params)->update(['qrcode_id' => $qrcode_id]);
+
+        return $this->successJson('ok');
+    }
+
+
+    //参与扫码记录
+    protected function userJoin($activity_id, $_W)
+    {
+        $params = array(
+            'uniacid' => \YunShop::app()->uniacid,
+            'code_id' => $activity_id,
+            'ip' => $_W['clientip'],
+            //'container' => $_W['container'],
+            //'os' => $_W['os'],
+            //'openid' => $_W['openid']
+        );
+
+        $lockCacheKey = 'userJoin' . \YunShop::app()->uniacid . $activity_id . $_W['clientip'] . date('Y-m-d H:i:s');
+
+        $lockCacheRs = Redis::setnx($lockCacheKey, 1);
+        if ($lockCacheRs != 1) {
+            return false;
+        }
+        Redis::expire($lockCacheKey, 5);
+
+        $data = [
+            'uniacid' => \YunShop::app()->uniacid,
+            'code_id' => $activity_id,
+            'ip' => $_W['clientip'],
+            'container' => $_W['container'],
+            'os' => $_W['os'],
+            'openid' => $_W['openid'] ? $_W['openid'] : ''
+        ];
+        ActivityUser::firstOrCreate($params, $data);
+
+    }
+
+}
