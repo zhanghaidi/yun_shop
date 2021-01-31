d246be0ee110ca29ac1c78de3b366bcd4ca05cf2
zhd <398978233@qq.com>
2021-01-31 15:46:24 +0800

    小程序文章不自动推荐

--- plugins/minapp-content/views/admin/hot_spot/list.blade.php
+++ plugins/minapp-content/views/admin/hot_spot/list.blade.php
@@ -9,11 +9,11 @@
         
             <div class="top" style="margin-bottom:20px">
                 <ul class="add-shopnav" id="myTab">
-                    <li><a href="{{yzWebUrl('plugin.minapp-content.admin.banner.index')}}">轮播图列表</a></li>
+                    <li ><a href="{{yzWebUrl('plugin.minapp-content.admin.banner.index')}}">轮播图列表</a></li>
                     <li><a href="{{yzWebUrl('plugin.minapp-content.admin.banner-position.index')}}">轮播位</a></li>
                     <li><a href="{{yzWebUrl('plugin.minapp-content.admin.system-category.index')}}">首页功能区分类</a></li>
-                    <li class="active"><a href="{{yzWebUrl('plugin.minapp-content.admin.system-image.index')}}">系统图片</a></li>
-                    <li><a href="{{yzWebUrl('plugin.minapp-content.admin.hot-spot.index')}}">首页热区</a></li>
+                    <li><a href="{{yzWebUrl('plugin.minapp-content.admin.system-image.index')}}">系统图片</a></li>
+                    <li class="active"><a href="{{yzWebUrl('plugin.minapp-content.admin.hot-spot.index')}}">首页热区</a></li>
                 </ul>
             </div>
 
@@ -43,7 +43,7 @@
                 </div>
             </form>
             <div class="col-xs-12 col-sm-2 col-md-2 col-lg-2">
-                <a href="{{ yzWebUrl('plugin.minapp-content.admin.system-image.edit') }}" class="btn btn-info">添加图片</a>
+                <a href="{{ yzWebUrl('plugin.minapp-content.admin.hot-spot.edit') }}" class="btn btn-info">添加热区</a>
             </div>
         </div>
     </div>
@@ -53,15 +53,11 @@
             <table class="table">
                 <thead>
                     <tr>
+                        <th width="100">ID</th>
                         <th width="100">排序</th>
-                        <th width="100">图片</th>
-                        <th>名称</th>
-                        <th>描述</th>
-                        <th>所属类型</th>
+                        <th>热区标题</th>
+                        <th>显示样式</th>
                         <th>是否显示</th>
-                        <th>跳转地址</th>
-                        <th>跳转类型</th>
-                        <th>所属平台</th>
                         <th>添加时间</th>
                         <th class="text-right">操作</th>
                     </tr>
@@ -70,14 +66,18 @@
                     @foreach($data as $value)
                     <tr>
                         <td>{{$value['list_order']}}</td>
+                        <td>{{$value['title']}}</td>
                         <td>
-                            <a href="{{tomedia($value['image'])}}" target="_blank"><img src="{{tomedia($value['image'])}}" width="50" border="1"></a>
+
+                                @if($value['type'] == 1)
+                                    <span class="label label-primary">横版</span>
+                                @else
+                                    <span class="label label-success">竖版</span>
+                                @endif
+
                         </td>
-                        <td>{{$value['name']}}</td>
-                        <td>{{$value['description']}}</td>
-                        <td>{{$value['cid']}}</td>
                         <td>
-                            <a href="{{ yzWebUrl('plugin.minapp-content.admin.system-image.status', ['id' => $value['id']]) }}">
+                            <a href="{{ yzWebUrl('plugin.minapp-content.admin.hot-spot.status', ['id' => $value['id']]) }}">
                             @if($value['status'] == 1)
                                 <span class="label label-primary">显示</span>
                             @else
@@ -85,25 +85,10 @@
                             @endif
                             </a>
                         </td>
-                        <td>{{$value['jumpurl']}}</td>
-                        <td>
-                        @if($value['jumptype'] == 1)
-                        普通页跳转
-                        @else
-                        导航跳转
-                        @endif
-                        </td>
-                        <td>
-                        @if($value['aid'] == 3)
-                        仙草集团公众号
-                        @elseif($value['aid'] == 45)
-                        艾居益小程序
-                        @endif
-                        </td>
                         <td>{{$value['create_time']}}</td>
                         <td class="text-right">
-                            <a href="{{ yzWebUrl('plugin.minapp-content.admin.system-image.edit', ['id' => $value['id']]) }}" title="编辑"><i class="fa fa-edit"></i></a> &nbsp; 
-                            <a href="{{ yzWebUrl('plugin.minapp-content.admin.system-image.delete', ['id' => $value['id']]) }}" onclick="return confirm('确定删除吗');return false;"  title="删除"><i class="fa fa-trash-o"></i></a>
+                            <a href="{{ yzWebUrl('plugin.minapp-content.admin.hot-spot.edit', ['id' => $value['id']]) }}" title="编辑"><i class="fa fa-edit"></i></a> &nbsp;
+                            <a href="{{ yzWebUrl('plugin.minapp-content.admin.hot-spot.delete', ['id' => $value['id']]) }}" onclick="return confirm('确定删除吗');return false;"  title="删除"><i class="fa fa-trash-o"></i></a>
                         </td>
                     </tr>
                     @endforeach
