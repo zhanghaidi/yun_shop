8edeccd9373c28f3b97f76055f4c7527788ef581
zhd <398978233@qq.com>
2021-02-03 12:09:48 +0800

    移除租赁插件

--- plugins/lease/views/admin/tabs.blade.php
+++
@@ -1,6 +0,0 @@
-<div  class="top">
-    <ul class="add-shopnav" id="myTab">
-        <li class="active" ><a href="#tab_lease_toy" class="tab">基础设置</a></li>
-        <li><a href="#tab_notice" class="tab">消息</a></li>
-    </ul>
-</div>
