cfe979f2489a377a9ed09ef502d9269a692ae0b7
张海地 <398978233@qq.com>
2021-04-08 14:32:14 +0800

    Delete jushuitan.php

--- config/jushuitan.php
+++
@@ -1,6 +0,0 @@
-<?php
-return [
-    'partnerid' => env('JU_PID','cf55396f7b9ef2ed82dddbb7c53bda09'),
-    'token' => env('JU_TON','04b5ce2d325983acb1feb5228665d12e'),
-    'partnerkey' => env('JU_KEY','8b2df22d4e18c598998437b94e632e3a')
-];
